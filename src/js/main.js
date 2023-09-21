5b76e69ec06c4988c08cb354d276d2ce460b71ed
vladi965 <manolomunoz0596@gmail.com>
2023-09-21 17:49:27 -0500

    Aun falta terminar

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+var myFullpage = new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    scrollHorizontally: true,
+    navigation: false,
+    css3: true,
+    anchors: ['firstPage', 'secundPage', 'thirdPage', 'fourthPage'],
+    navigationTooltis: ['first', 'secund', 'third', 'fourth'],
+
+});
